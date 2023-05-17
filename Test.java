19cb10548f8d82142314b6ad8420a70462cc7b89
Soufiane Adadi <adadi@outlook.com>
2023-05-17 17:35:51 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,18 +1,64 @@
-import java.security.SecureRandom;
+import io.cucumber.java.en.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
-public String generatePassword() {
-    int length = 10;
-    String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    SecureRandom random = new SecureRandom();
-    StringBuilder sb = new StringBuilder(length);
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
-    for (int i = 0; i < length; i++) {
-        int randomIndex = random.nextInt(characters.length());
-        char randomChar = characters.charAt(randomIndex);
-        sb.append(randomChar);
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class CaseServiceSteps {
+
+    @Autowired
+    private ICaseService caseService;
+
+    private LocalDate startBookingDate;
+    private LocalDate endBookingDate;
+    private Set<Integer> caseIds;
+    private Mono<List<CaseExternalDto>> casesResult;
+    private Mono<LastUpdateDto> lastUpdateResult;
+
+    @Given("I have the start booking date {string}")
+    public void iHaveTheStartBookingDate(String startBookingDate) {
+        this.startBookingDate = LocalDate.parse(startBookingDate);
     }
 
-    String generatedString = sb.toString();
-    System.out.println("Generated password: " + generatedString);
-    return generatedString;
+    @Given("I have the end booking date {string}")
+    public void iHaveTheEndBookingDate(String endBookingDate) {
+        this.endBookingDate = LocalDate.parse(endBookingDate);
+    }
+
+    @Given("I have the following case IDs:")
+    public void iHaveTheFollowingCaseIDs(List<Integer> caseIds) {
+        this.caseIds = new HashSet<>(caseIds);
+    }
+
+    @When("I call the getCases method")
+    public void iCallTheGetCasesMethod() {
+        casesResult = caseService.getCases(startBookingDate, endBookingDate);
+    }
+
+    @When("I call the getCaseByIds method")
+    public void iCallTheGetCaseByIdsMethod() {
+        casesResult = caseService.getCaseByIds(caseIds);
+    }
+
+    @When("I call the getLastUpdate method")
+    public void iCallTheGetLastUpdateMethod() {
+        lastUpdateResult = caseService.getLastUpdate();
+    }
+
+    @Then("I should receive a list of CaseExternalDto")
+    public void iShouldReceiveAListOfCaseExternalDto() {
+        assertNotNull(casesResult);
+        casesResult.subscribe(caseExternalDtos -> assertFalse(caseExternalDtos.isEmpty()));
+    }
+
+    @Then("I should receive a LastUpdateDto")
+    public void iShouldReceiveALastUpdateDto() {
+        assertNotNull(lastUpdateResult);
+    }
 }
